f98afe702f848c39cf237c17eabd0e805286b234
oliverowen2210 <oliverowen2210@gmail.com>
2022-11-09 03:14:29 -0600

    reimplemented targeting in react

--- src/components/Waldo.js
+++ src/components/Waldo.js
@@ -4,10 +4,57 @@ import Target from "./Target";
 export default function Waldo(props) {
   let [targeting, setTargeting] = useState(false);
   let [targetCoords, setTargetCoords] = useState([0, 0]);
+  let [targetSize, setTargetSize] = useState(null);
+  let waldoRef = useRef(null);
+
+  let image = new Image();
+  image.src = props.image;
+  let ratio = image.naturalWidth / image.naturalHeight;
+
+  function placeTarget({ size, x, y }) {
+    setTargetSize(size);
+    let offset = targetSize / 2;
+    setTargetCoords([x - offset + "px", y - offset + "px"]);
+    setTargeting(true);
+    let hideTargetTimeout = setTimeout(() => {
+      setTargeting(false);
+    }, 150);
+  }
+
+  function handleClick(e) {
+    let waldoWidth = waldoRef.current.offsetWidth;
+    let waldoHeight = waldoRef.current.offsetHeight;
+
+    let clickX = e.offsetX;
+    let clickY = e.offsetY;
+
+    let x = 100 * parseFloat(clickX / waldoWidth).toFixed(2);
+    let y = 100 * parseFloat(clickY / waldoHeight).toFixed(2);
+
+    console.log(x, y);
+    placeTarget({
+      size: waldoWidth * 0.04,
+      x: clickX,
+      y: clickY,
+    });
+  }
   return (
     <div className="waldo">
-      <img src={props.image} className="waldoImage" />
-      <Target visible={targeting} x={targetCoords[0]} y={targetCoords[1]} />;
+      <img
+        ref={waldoRef}
+        src={props.image}
+        className="waldoImage"
+        onClick={(e) => {
+          handleClick(e.nativeEvent);
+        }}
+      />
+      <Target
+        visible={targeting}
+        x={targetCoords[0]}
+        y={targetCoords[1]}
+        size={targetSize}
+      />
+      ;
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -9,12 +9,6 @@ root.render(
   </React.StrictMode>
 );
 
-let waldo = document.getElementById("waldo");
-let target = document.getElementById("target");
-
-let waldoWidth = null;
-let waldoHeight = null;
-
 let coords = {
   waldo: {
     start: [78, 7],
@@ -34,37 +28,3 @@ function checkHit(x, y) {
     }
   }
 }
-
-function update() {
-  waldoWidth = waldo.offsetWidth;
-  waldoHeight = waldo.offsetHeight;
-
-  target.style.width = `${waldoWidth * 0.04}px`;
-  target.style.height = `${waldoWidth * 0.04}px`;
-}
-
-function placeTarget(x, y) {
-  let offSet = waldoWidth * 0.02;
-  target.classList.remove("hidden");
-  target.style.left = x - offSet + "px";
-  target.style.top = y - offSet + "px";
-  let hideTargetTimeout = setTimeout(() => {
-    target.classList.add("hidden");
-  }, 150);
-}
-
-waldo.addEventListener("click", (e) => {
-  if (waldo.offsetWidth != waldoWidth || waldoHeight != waldo.offsetHeight) {
-    update();
-  }
-
-  let clickX = e.offsetX;
-  let clickY = e.offsetY;
-
-  let x = 100 * parseFloat(clickX / waldoWidth).toFixed(2);
-  let y = 100 * parseFloat(clickY / waldoHeight).toFixed(2);
-
-  console.log(x, y);
-  placeTarget(e.clientX, e.clientY);
-  checkHit(x, y);
-});
